ce0c207248ea740506ad71160749db15c23228f2
MrOtherGuy <jastekken@outlook.com>
2022-03-19 12:59:09 +0200

    enable aspect-ratio, overflow-block, overflow-inline, prefers-contrast
    This patch also makes value boxes stand out a bit more and changes them
    to <pre> elements replacing <div>s

--- generate-html.js
+++ generate-html.js
@@ -2,52 +2,53 @@ const fs = require('fs').promises;
 
 function createThing(name,obj,textObject){
 
-  textObject.htmlText += `<div id="media-${name}" class="media-item">\n<h2>${name}</h2>\n<div class="description">${obj.decription}`
-  if(obj.range && obj.range.unit){
+  textObject.htmlText += `<div id="media-${name}" class="media-item">\n<h2>${name}</h2>\n<div class="description">${obj.description}`
+  if(obj.range && (obj.range.unit || obj.range.fraction)){
     textObject.htmlText += ` (${obj.range.unit})`;
   }
   textObject.htmlText += `</div>\n`;
   // item <div> is open
-  
   if(obj.range){
     
-    const isSparse = obj.range.step > 1;
+    const isSparse = (obj.range.step > 1) || obj.range.fraction;
 
     if(isSparse){
-      textObject.htmlText += `<div class="range sparse">\n<div><span>0</span></div>\n`;
+      textObject.htmlText += `<pre class="range sparse">\n<div><span>0</span></div>\n`;
     }else{
-      textObject.htmlText += `<div class="range">\n<div>0</div>\n`;
+      textObject.htmlText += `<pre class="range">\n<div>0</div>\n`;
     }
     // range div is open
  
-    let maxn = 15;
+    let maxn = 20;
     const N = Math.floor((obj.range.max - obj.range.min)/obj.range.step) + 1;
     let k = 1;
     let i = obj.range.min;
     const rangeSelector = `#media-${name} > .range`;
     textObject.cssText += `${rangeSelector}{ background-size: ${100/(N+1)}% }\n`;
-    textObject.cssText += `@media (max-${name}:${obj.range.min}${obj.range.unit}){ ${rangeSelector}{ background-position: 0% } }\n`;
+    const suffix = obj.range.fraction ? `/${obj.range.denominator}` : obj.range.unit;
+    textObject.cssText += `@media (max-${name}:${obj.range.min}${suffix}){ ${rangeSelector}{ background-position: 0% } }\n`;
     
     while( maxn > 0 && i <= obj.range.max){
-
+      
       if(isSparse){
-        textObject.htmlText += `<div><span>${i}</span></div>\n`;
+        let text = obj.range.fraction ? i + suffix : i; 
+        textObject.htmlText += `<div><span>${text}</span></div>\n`;
       }else{
         textObject.htmlText += `<div>${i}</div>\n`;
       }
       
-      textObject.cssText += `@media (min-${name}:${i}${obj.range.unit}){ ${rangeSelector}{ background-position: ${k/N*100}% } }\n`;
+      textObject.cssText += `@media (min-${name}:${i}${suffix}){ ${rangeSelector}{ background-position: ${k/N*100}% } }\n`;
       
       maxn--;
       i+=obj.range.step;
       k++;
     }
 
-    textObject.htmlText += `</div>\n`; // closes range div
+    textObject.htmlText += `</pre>\n`; // closes range div
   }else{
   
     for(let n of obj.values){
-      textObject.htmlText += `<div class="v_${n}">${n}${obj.unit?obj.unit:""}</div>`
+      textObject.htmlText += `<pre class="v_${n}">${n}${obj.unit?obj.unit:""}</pre>`
 
       textObject.cssText += `@media (${name}${n===""?"":":"}${n}){\n#media-${name} > .v_${n}{ display: block }\n}\n`
     }
@@ -70,7 +71,7 @@ function createTexts(db){
 </head>
 <body>
 <h1>CSS media-query test page</h1>
-<p>Shows values reported by css media-queries. Everything is done with css, no javascript required.</p>
+<p>Shows values reported by css media-queries. Everything is done with css, no javascript required. Lack of "value-box" implies the media-query is not supported by the browser.</p>
 <div>
 `,
    "cssText":`/* Generated stylesheet */\n`
